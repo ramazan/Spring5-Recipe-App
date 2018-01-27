1f17d8f4873fd7bf71959d1a6e4177990be3b66a
Ramazan Demir <ramazan.demir@sahibinden.com>
2018-01-27 23:27:49 +0300

    initial commit

---
+++ src/main/java/com/ramazan/recipeapp/RecipeAppApplication.java
@@ -0,0 +1,12 @@
+package com.ramazan.recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ramazan/recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,17 @@
+package com.ramazan.recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String uom;
+
+}
