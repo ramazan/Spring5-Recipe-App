9449db207e1ba1ab6501c2d7cdde4aa7c1c3733b
Ramazan Demir <ramazan.demir@sahibinden.com>
2018-01-28 16:45:26 +0300

    Project Lombok Added. Refactoring code

--- src/main/java/com/ramazan/recipeapp/domain/Recipe.java
+++ src/main/java/com/ramazan/recipeapp/domain/Recipe.java
@@ -1,10 +1,11 @@
 package com.ramazan.recipeapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,9 +18,10 @@ import javax.persistence.OneToOne;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
-public class Recipe
-{
+public class Recipe {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,149 +36,34 @@ public class Recipe
     @Lob
     private String directions;
 
-    @Enumerated(value = EnumType.STRING)
-    private Difficulty difficulty;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
     @ManyToMany
-    @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
-
-    public Long getId()
-    {
-        return id;
-    }
-
-    public void setId(Long id)
-    {
-        this.id = id;
-    }
-
-    public String getDescription()
-    {
-        return description;
-    }
-
-    public void setDescription(String description)
-    {
-        this.description = description;
-    }
-
-    public Integer getPrepTime()
-    {
-        return prepTime;
-    }
-
-    public void setPrepTime(Integer prepTime)
-    {
-        this.prepTime = prepTime;
-    }
-
-    public Integer getCookTime()
-    {
-        return cookTime;
-    }
-
-    public void setCookTime(Integer cookTime)
-    {
-        this.cookTime = cookTime;
-    }
-
-    public Integer getServings()
-    {
-        return servings;
-    }
-
-    public void setServings(Integer servings)
-    {
-        this.servings = servings;
-    }
-
-    public String getSource()
-    {
-        return source;
-    }
-
-    public void setSource(String source)
-    {
-        this.source = source;
-    }
-
-    public String getUrl()
-    {
-        return url;
-    }
-
-    public void setUrl(String url)
-    {
-        this.url = url;
-    }
-
-    public String getDirections()
-    {
-        return directions;
-    }
-
-    public void setDirections(String directions)
-    {
-        this.directions = directions;
-    }
-
-    public Byte[] getImage()
-    {
-        return image;
-    }
-
-    public void setImage(Byte[] image)
-    {
-        this.image = image;
-    }
-
-    public Notes getNotes()
-    {
-        return notes;
-    }
 
     public void setNotes(Notes notes)
     {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
-    public Set<Ingredient> getIngredients()
+    public Recipe addIngredient(Ingredient ingredient)
     {
-        return ingredients;
-    }
-
-    public void setIngredients(Set<Ingredient> ingredients)
-    {
-        this.ingredients = ingredients;
-    }
-
-    public Difficulty getDifficulty()
-    {
-        return difficulty;
-    }
-
-    public void setDifficulty(Difficulty difficulty)
-    {
-        this.difficulty = difficulty;
-    }
-
-    public Set<Category> getCategories()
-    {
-        return categories;
-    }
-
-    public void setCategories(Set<Category> categories)
-    {
-        this.categories = categories;
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 }
